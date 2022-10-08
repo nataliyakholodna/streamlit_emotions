de82ac9474c08313f6a5e029db5be34daa1b965c
nataliyakholodna <kholodnaya.nataliya@gmail.com>
2022-10-08 23:44:19 +0200

    added comments to app.py

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ import pandas as pd
 import datetime
 import os
 
+from tensorflow import keras
+import pickle
+
 from utils.get_data import collect_tweets, load_csvs
 from utils.charts import show_table, show_bar
 from utils.preprocess import clean_tweets, text_preprocess
@@ -10,17 +13,9 @@ from utils.predict import predict
 from utils.utils import del_folder_content, most_popular_to_days
 from constants import *
 
-from tensorflow import keras
-import pickle
-
 import plotly.graph_objects as go
 
 TODAY = datetime.date.today()
-
-# Remove previous data files if any
-# del_folder_content(DATA_PATH)
-# del_folder_content(PREPROCESSED_DATA_PATH)
-# del_folder_content(EMOTION_COUNTS_PATH)
 
 # Banner
 st.image('images/banner.png')
@@ -39,7 +34,7 @@ with st.form('form1'):
 
     # Additional search params
     with st.expander('Other search settings'):
-        # Hashtags, likes, num tweets
+        # Hashtags, min likes, max num of tweets
         hashtags_checkbox = st.checkbox('Only tweets with hashtags', False)
         col1, col2 = st.columns(2)
         num_tweets_per_day = int(col1.number_input('Max number of tweets per day',
@@ -48,7 +43,7 @@ with st.form('form1'):
         min_favs = int(col1.number_input('Min likes per tweet',
                                          value=0, step=1,
                                          min_value=0))
-        # Location
+        # Location by name or coordinates
         location = st.text_input('Location:')
         col1, col2 = st.columns(2)
         coordinates = col1.text_input('Coordinates:')
@@ -57,23 +52,20 @@ with st.form('form1'):
     # Search button
     search_btn = st.form_submit_button('🔍 Search!')
 
-# ---  Session variables  ------------------------------------------------------
-
-# if 'search' not in st.session_state:
-#     st.session_state.search = False
-# if 'analyze' not in st.session_state:
-#     st.session_state.analyze = False
 
 # ---  Collect data  -----------------------------------------------------------
 if search_btn:
+    # Start date can not be later that end date
     if end_date <= begin_date:
         st.error('Incorrect date!')
     else:
-        # st.session_state.search = True
+        # Split keywords into a list
         keyword_list = keyword.split(',')
-        # Split on whitespace and join words to delete space after the words_
+        # Remove extra whitespaces for each keyword
         keyword_list = [k.strip() for k in keyword_list]
 
+        # Collect tweets and save them to dict in the following format:
+        # {'YYYY-MM-DD': pd.Dataframe(columns=['date', 'content'])}
         dfs = collect_tweets(keywords=keyword_list,
                              only_hashtags=hashtags_checkbox,
                              num_tweets_per_day=num_tweets_per_day,
@@ -81,46 +73,48 @@ if search_btn:
                              begin_date=begin_date,
                              end_date=end_date)
 
-        # Remove previous results
+        # Remove previous results (files) af any
         del_folder_content(DATA_PATH)
         del_folder_content(PREPROCESSED_DATA_PATH)
         del_folder_content(EMOTION_COUNTS_PATH)
 
-        # Save collected raw data, one dataframe per day
+        # Save collected raw data, named in format 'YYYY-MM-DD.csv'
         for name, df in dfs.items():
             df.to_csv(DATA_PATH + '/' + name + '.csv', index=False)
 
-# ---  Load Data  --------------------------------------------------------------
 
-# Load csv files
-# Dict in format {'name': pd.DataFrame}, list of names of the files
+# ---  Load RAW Data  ----------------------------------------------------------
 if any(os.scandir(DATA_PATH)):
+    # 1: Dict in format {'YYYY-MM-DD': pd.Dataframe(columns=['date', 'content'])}
+    # 2: List of the file names (['YYYY-MM-DD', ...])
     dfs, date_options = load_csvs(DATA_PATH)
 
-    # Show number of collected tweets per day via bar chart
+    # Count number of tweets collected every day
     count_tweets = {k: len(df) for k, df in dfs.items()}
+    st.markdown('##')  # Space before element
     st.write('\nThe following number of tweets was found:')
     col1, col2 = st.columns([2, 1])
+    # Show number of collected tweets per day via bar chart
     fig = show_bar(count_tweets.keys(), count_tweets.values())
     col1.plotly_chart(fig, use_container_width=True)
-    # Convert counts to dataframe and right to bar chart
+    # Convert counts to dataframe and display to the right side to bar chart
     col2.dataframe(pd.DataFrame.from_dict(count_tweets, orient='index'), height=180)
 
     # Show records for a particular day
     if date_options:
+        st.markdown('##')
         day_selected = st.selectbox(label='View by date:', options=date_options)
-
         if day_selected in dfs:
+            # Select the corresponding dataframe
             df = dfs[day_selected]
             # Show table
             fig = show_table(df)
             st.plotly_chart(fig, use_container_width=True)
 
+    st.markdown('##')
     if st.button('📈 Analyze!'):
 
-        # st.session_state.analyze = True
-
-        # ---  Count Records  ------------------------------------------------------
+        # ---  Preprocess  -----------------------------------------------------
         # Clean tweets: remove hashtags, mentions and other symbols
         dfs_cleaned = {}
         for day in date_options:
@@ -133,8 +127,11 @@ if any(os.scandir(DATA_PATH)):
                                           df['content_cleaned']]
             df['content_preprocessed_with_stopwords'] = [text_preprocess(t) for t in df['content_cleaned']]
 
+        # ---  Obtain Predictions  -----------------------------------------------------
         with st.spinner('Predicting...'):
+            # Load pretrained model
             model = keras.models.load_model('models/lstm/content/lstm_model')
+            # Load pretrained tokenizer
             with open('tokenizers/lstm/tokenizer.pickle', 'rb') as t:
                 tokenizer = pickle.load(t)
 
@@ -142,9 +139,12 @@ if any(os.scandir(DATA_PATH)):
             hashtags = []
 
             for i, day in enumerate(date_options):
+                # Predict labels for each record in the dataframe
                 emotions = predict(dfs_cleaned[day]['content_preprocessed_with_stopwords'], model, tokenizer)
+                # Add decoded predicted labels to the original dataframe
                 dfs[day]['predicted_labels'] = [LABELS_TO_EMOTIONS[i] for i in emotions]
 
+                # Count number of each emotion type for the current date
                 no_emotion.append(emotions.count(0))
                 anger.append(emotions.count(1))
                 fear.append(emotions.count(3))
@@ -152,6 +152,7 @@ if any(os.scandir(DATA_PATH)):
                 sadness.append(emotions.count(5))
                 surprise.append(emotions.count(6))
 
+                # Calculate most popular words or hashtags for the current day
                 words_to_days = most_popular_to_days(dfs_cleaned,
                                                      date_options,
                                                      'words',
@@ -159,9 +160,11 @@ if any(os.scandir(DATA_PATH)):
                                                      min_occur=1)
                 # st.write(words_to_days)
 
+                #
                 hash_today = words_to_days[day]['word'].values
                 hashtags.append(', '.join(hash_today))
 
+            # Create a dataframe with emotion counts
             emotion_counts = pd.DataFrame({
                 'Date': date_options,
                 'no emotion': no_emotion,
@@ -177,20 +180,23 @@ if any(os.scandir(DATA_PATH)):
         del_folder_content(PREPROCESSED_DATA_PATH)
         del_folder_content(EMOTION_COUNTS_PATH)
 
-        # Save dataframes as pdf, one per day
+        # Replace original datafiles by datafiles with predictions
         for name, df in dfs.items():
             df.to_csv(DATA_PATH + '/' + name + '.csv', index=False)
 
-        # Save dataframes as pdf, one per day
+        # Save preprocessed data
         for name, df in dfs_cleaned.items():
             df.to_csv(PREPROCESSED_DATA_PATH + '/' + name + '.csv', index=False)
 
+        # Save counts ('Date', '# no_emotion', '# happiness', ...)
         emotion_counts.to_csv(EMOTION_COUNTS_PATH + '/emotion_counts.csv', index=False)
 
 if any(os.scandir(EMOTION_COUNTS_PATH)):
     emotion_counts = pd.read_csv(EMOTION_COUNTS_PATH + '/emotion_counts.csv')
     st.dataframe(emotion_counts)
-    emotion_counts.hashtags = emotion_counts.hashtags.astype(str)
+
+    if emotion_counts.hashtags.isnull().all():
+        emotion_counts.hashtags = emotion_counts.hashtags.astype(str)
 
     ###############
     ###  TO DO  ###
@@ -259,5 +265,7 @@ if any(os.scandir(EMOTION_COUNTS_PATH)):
                              hovertemplate=hover,
                              text=hvr, fill='tozeroy'))
 
-    fig.update_layout(template='none', hovermode='closest')
+    fig.update_layout(hovermode='closest',
+                      margin=dict(l=0, r=0),
+                      )
     st.plotly_chart(fig, use_container_width=True)
